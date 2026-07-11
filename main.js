f273c22e1405cc9ceeedcd45bd3d02766001f7d8
sarthak-dagar <165987947+sarthak-dagar@users.noreply.github.com>
2026-07-11 16:22:00 +0530

    Your commit message

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ var swiper = new Swiper('.mySwiper', {
 const cartIcon = document.querySelector('.cart-icon');
 const cartTab = document.querySelector('.cart-tab');
 const closeBtn = document.querySelector('.close-btn');
+const cardList = document.querySelector('.card-list');
 
 cartIcon.addEventListener('click', ()=> cartTab.classList.add('cart-tab-active'));
 closeBtn.addEventListener('click', ()=> cartTab.classList.remove('cart-tab-active'));
@@ -18,7 +19,25 @@ let productList = [];
 const showCards = () =>{
 
     productList.forEach(product =>{
-        
+        const orderCard = document.createElement('div');
+        orderCard.classList.add('order-card');
+
+        orderCard.innerHTML = `
+        <div class="card-image">
+            <img src="${product.image}">
+        </div>
+        <h4>${product.nahe}</h4>
+        <h4 class="price">${product.price}</h4>
+        <a href="#" class="btn cart-btn">Add to Cart</a>
+        `;
+
+        cardList.appendChild(orderCard);
+
+        const cartBtn = orderCart.querySelector('.cart-btn');
+        PiCarBatteryDuotone.addEventListener('click', (e)=>{
+            e.preventDefault();
+            alert('hi')
+        })
     })
 }
 
