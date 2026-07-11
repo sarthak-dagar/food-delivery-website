528996a8a7f1c186d7f648808844cecb577afce4
sarthak-dagar <165987947+sarthak-dagar@users.noreply.github.com>
2026-07-11 14:52:57 +0530

    Your commit message

--- main.js
+++ main.js
@@ -5,3 +5,33 @@ var swiper = new Swiper('.mySwiper', {
         prevEl: '#prev',
     },
 });
+
+const cartIcon = document.querySelector('.cart-icon');
+const cartTab = document.querySelector('.cart-tab');
+const closeBtn = document.querySelector('.close-btn');
+
+cartIcon.addEventListener('click', ()=> cartTab.classList.add('cart-tab-active'));
+closeBtn.addEventListener('click', ()=> cartTab.classList.remove('cart-tab-active'));
+
+let productList = [];
+
+const showCards = () =>{
+
+    productList.forEach(product =>{
+        
+    })
+}
+
+const initApp = () =>{
+
+    fetch('product.json').then
+    (response => response.json()).then
+    (data =>{
+
+        productList = data;
+        showCards();
+    })
+}
+
+initApp();
+
