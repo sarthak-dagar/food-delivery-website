092d0add86c03a7e304798ff8cb35c52802acaaa
sarthak-dagar <165987947+sarthak-dagar@users.noreply.github.com>
2026-07-23 23:38:08 +0530

    Your commit message

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -4,7 +4,7 @@ const productSchema = new mongoose.Schema({
     id: { type: Number, required: true, unique: true },
     name: { type: String, required: true },
     price: { type: String, required: true },
-    image: { type: String, required: true }
+    image: { type: String, required: true }    
 });
 
 module.exports = mongoose.modek('Product', productSchema);    
