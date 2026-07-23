9e252605b9f3b5b779d2ce141e1ba5ae9ea955be
sarthak-dagar <165987947+sarthak-dagar@users.noreply.github.com>
2026-07-23 23:38:27 +0530

    Your commit message

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    id: { type: Number, required: true, unique: true },
+    id: { type: Number, required: true, unique: true },   
     name: { type: String, required: true },
     price: { type: String, required: true },
     image: { type: String, required: true }    
