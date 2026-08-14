9e6718918f36568249aa7456dac190b4e2a7ac2b
sarthak-dagar <165987947+sarthak-dagar@users.noreply.github.com>
2026-08-14 20:57:51 +0530

    Your commit message

--- backend/server.js
+++ backend/server.js
@@ -28,4 +28,4 @@ app.use(express.static(path.join(__dirname, '..')));
 const PORT = process.env.PORT || 5000;
 
 app.use((req, res) => res.status(404).json({ message: 'Route not found' }));     
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));   
