fe3cb73b67cdca00e90ac9474ec8d1c8d5ec6e24
sarthak-dagar <165987947+sarthak-dagar@users.noreply.github.com>
2026-08-16 21:06:09 +0530

    message

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use('/api/orders', require('./routes/orders'));
 
 app.use(express.static(path.join(__dirname, '..')));
 
-const PORT = process.env.PORT || 5000;   
+const PORT = process.env.PORT || 5000;      
 
 app.use((req, res) => res.status(404).json({ message: 'Route not found' }));     
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));   
