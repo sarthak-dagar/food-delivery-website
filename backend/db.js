b83e8e8f3dac74a55b0c5184c5cc087a25a4d495
sarthak-dagar <165987947+sarthak-dagar@users.noreply.github.com>
2026-09-22 23:35:53 +0530

    message

--- backend/db.js
+++ backend/db.js
@@ -6,7 +6,7 @@ const SCHEMA = `
     id TEXT PRIMARY KEY,
     name TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL,
+    password TEXT NOT NULL,    
     createdAt TEXT NOT NULL
   );
 
